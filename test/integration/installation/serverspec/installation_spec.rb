918b1c8d36429a3aa700b9a33fcd97ce91fbd0e5
Ross Timson <ross@rosstimson.com>
2014-03-23 12:52:18 +0000

    Adding test for ruby-install version bump.

--- test/integration/installation/serverspec/installation_spec.rb
+++ test/integration/installation/serverspec/installation_spec.rb
@@ -2,7 +2,7 @@
 
 require_relative 'spec_helper'
 
-describe 'ruby-install 0.4.0 is installed at /usr/local/bin/ruby-install' do
+describe 'ruby-install 0.4.1 is installed at /usr/local/bin/ruby-install' do
   describe file('/usr/local/bin/ruby-install') do
     it { should be_file }
     it { should be_owned_by 'root' }
@@ -11,6 +11,6 @@ describe 'ruby-install 0.4.0 is installed at /usr/local/bin/ruby-install' do
   end
   describe command('/usr/local/bin/ruby-install --version') do
     it { should return_exit_status 0 }
-    it { should return_stdout 'ruby-install: 0.4.0' }
+    it { should return_stdout 'ruby-install: 0.4.1' }
   end
 end
