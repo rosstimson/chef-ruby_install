b7ced9adf4cdefdf97b628fab4c81a1fbd5b0943
Ross Timson <ross@rosstimson.com>
2014-03-24 18:42:26 +0000

    Refactor long method into two.

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -41,29 +41,30 @@ private
 
 def perform_install
   if ruby_installed?
-    Chef::Log.debug(
-      "ruby_install_ruby[#{@rubie}] is already installed, so skipping")
+    Chef::Log.debug("ruby_install_ruby[#{@rubie}] is already installed, so skipping")
   else
     install_start = Time.now
-
-    Chef::Log.info(
-      "Building ruby_install_ruby[#{@rubie}], this could take a while...")
+    Chef::Log.info("Building ruby_install_ruby[#{@rubie}], this could take a while...")
 
     rubie       = @rubie        # bypass block scoping issue
     prefix_path = @prefix_path  # bypass block scoping issue
-    execute "ruby-install[#{rubie}]" do
-      command   %( /usr/local/bin/ruby-install
-                   --rubies-dir "#{prefix_path}" "#{rubie}" )
-      user        new_resource.user         if new_resource.user
-      group       new_resource.group        if new_resource.group
-      environment new_resource.environment  if new_resource.environment
-      action    :nothing
-    end.run_action(:run)
+
+    execute_ruby_install(rubie, prefix_path)
 
     Chef::Log.info("ruby_install_ruby[#{@rubie}] build time was " \
       "#{(Time.now - install_start) / 60.0} minutes")
     new_resource.updated_by_last_action(true)
   end
+end
+
+def execute_ruby_install(rubie, prefix_path)
+  execute "ruby-install[#{rubie}]" do
+    command   %( /usr/local/bin/ruby-install --rubies-dir "#{prefix_path}" "#{rubie}" )
+    user        new_resource.user         if new_resource.user
+    group       new_resource.group        if new_resource.group
+    environment new_resource.environment  if new_resource.environment
+    action    :nothing
+  end.run_action(:run)
 end
 
 def ruby_installed?
